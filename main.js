f3e4e54042798c2f1ef95a13fe0d1bec90fbc14c
kaycho <kaycho@choui-MacBookPro.local>
2019-10-17 17:07:28 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const {app, Menu, Tray} = require('electron')
+const fs = require('fs')
+
+let trayMenu = null
+
+app.dock.hide()
+if (process.platform !== "darwin") win.setSkipTaskbar (true)
+
+app.on('ready', async () => {
+  trayMenu = new Tray(__dirname + '/src/tram.png')
+  console.log(menuInfo)
+  const contextMenu = Menu.buildFromTemplate([
+    {label:'menu1', type:'normal'},
+    {type:'separator'},
+    {label:'menu2',
+      submenu: [
+        {label:'submenu1', type:'normal'},
+        {label:'submenu2', type:'normal'}
+      ]
+    }
+  ])
+
+  trayMenu.setToolTip('Tram')
+  trayMenu.on('click', () => {
+    //popupcontextmenu 체크
+    trayMenu.popUpContextMenu(contextMenu)
+  })
+})
